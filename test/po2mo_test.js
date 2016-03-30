8087b448e03772206af5b402973ad34599397515
Antoine Martin <hyptos974@gmail.com>
2016-03-30 10:32:38 +0200

    Update po2mo_test.js
    Added multiple tests and modified other tests

--- test/po2mo_test.js
+++ test/po2mo_test.js
@@ -9,8 +9,9 @@ exports.po2mo = {
   stage: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/fr.mo');
-    var expected = grunt.file.read('test/expected/fr.mo');
+    var actual = grunt.file.read('tmp/fr/message.mo');
+    var expected = grunt.file.read('test/expected/fr/message.mo');
+
     test.equal(actual, expected);
 
     test.done();
@@ -18,9 +19,18 @@ exports.po2mo = {
   prod: function(test) {
     test.expect(1);
 
-    var expected = grunt.file.exists('tmp/fixtures/fr.po');
+    var expected = grunt.file.exists('tmp/fr/message.po');
     test.equal(expected, false);
 
     test.done();
   },
+  multiple: function(test){
+    test.expect(1);
+
+    var expectedMo = grunt.file.exists('test/fixtures/fr/message.mo');
+    test.equal(expectedMo, true);
+    grunt.file.delete('test/fixtures/fr/message.mo');
+
+    test.done();
+  }
 };
