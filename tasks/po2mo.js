9b35c0df5c0db22f43c7f7d00030633297e3cefc
Antoine Martin <hyptos974@gmail.com>
2016-03-30 09:19:46 +0200

    Update po2mo.js
    Managing multiple src and added option to remove dest. TODO : writing tests for new use case

--- tasks/po2mo.js
+++ tasks/po2mo.js
@@ -5,44 +5,52 @@
  * Copyright (c) 2013 Michele Bertoli
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 var exec = require('sync-exec');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.registerMultiTask('po2mo', 'Compile .po files into binary .mo files with msgfmt.', function() {
+    grunt.registerMultiTask('po2mo', 'Compile .po files into binary .mo files with msgfmt.', function () {
 
-    var options = this.options({
-      deleteSrc: false,
+        var options = this.options({
+            deleteSrc: false,
+        });
+
+        var renameFile = function (src, dest) {
+            if (dest.indexOf('.po') > -1) {
+                dest = dest.replace('.po', '.mo');
+            }
+
+            var command = 'msgfmt -o ' + dest + ' ' + src;
+
+            grunt.verbose.writeln('Executing: ' + command);
+            var result = exec(command);
+            grunt.verbose.writeln('Executed with status: ' + result.status);
+
+            if (result.status !== 0) {
+                grunt.log.error(result.stderr);
+            }
+        };
+
+        this.files.forEach(function (file) {
+            var arraySrc  = file.src;
+            var arrayDest = file.dest;
+
+            if (!arrayDest) {
+                for (var i = 0; i < arraySrc.length; i++) {
+                    var fileSrc = arraySrc[i];
+                    renameFile(fileSrc, fileSrc);
+                }
+            } else {
+                renameFile(arraySrc, arrayDest);
+            }
+
+            if (options.deleteSrc) {
+                grunt.file.delete(arraySrc);
+            }
+        });
+
     });
-
-    this.files.forEach(function(file) {
-
-      var src = file.src[0];
-      var dest = file.dest;
-      if (dest.indexOf('.po') > -1) {
-        dest = dest.replace('.po', '.mo');
-      }
-      grunt.file.write(dest);
-
-      var command = 'msgfmt -o ' + dest + ' ' + src;
-
-      grunt.verbose.writeln('Executing: ' + command);
-      var result = exec(command);
-      grunt.verbose.writeln('Executed with status: ' + result.status);
-
-      if (result.status !== 0) {
-        grunt.log.error(result.stderr);
-      }
-
-      if (options.deleteSrc) {
-        grunt.file.delete(src);
-      }
-
-    });
-
-  });
 
 };
